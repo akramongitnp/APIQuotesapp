a5dfd9568aa82c499ccc54c8376d619d3842d042
akramongitnp <akramsiddikinp@gmail.com>
2022-06-16 08:18:22 +0530

    Modal and login screen created but few bugs there

--- Components/main.js
+++ Components/main.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Modal, Alert, ToastAndroid } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
 import Tts from 'react-native-tts';
+import Clipboard from '@react-native-clipboard/clipboard';
+import { Button } from 'react-native-web';
+/*import Snackbar from 'react-native-snackbar';*/
 
-function Main() {
+function Main({navigation}) {
   const [isLoading, setLoading] = useState(false)
   const [Quote, setQuote] = useState('Loading...')
   const [Author, setAuthor] = useState('Loading...')
+  const [window, setwindow] = useState(false)
 
   const randomQuote = () => {
     setLoading(true)
@@ -23,8 +27,21 @@ function Main() {
       Tts.speak('Hello World');
     }
     catch {
-      console.log('Facing error')
+      Alert.alert('Error' ,'Something went wrong, our enginers are looking into this')
     }
+  }
+
+  const copyToClipboard = () => {
+    Clipboard.setString(Quote);
+    /*Snackbar.show({
+      text: 'Cpoied', 
+      duration: Snackbar.LENGTH_LONG,
+    });*/
+    ToastAndroid.show('Copied!', ToastAndroid.SHORT)
+  };
+
+  const popupWin = () => {
+    setwindow(true)
   }
 
   useEffect(() => {
@@ -37,17 +54,26 @@ function Main() {
             <Text style={styles.txtMain}>{isLoading ? 'Loading...' : Quote}</Text>
             <Text style={{marginLeft: '70%', color: '#A6F32C'}}>____{isLoading ? 'Loading...' : Author}</Text>
             <View style={styles.btnBox}>
-                <TouchableOpacity style={{padding: 15, borderRadius: 20, backgroundColor: isLoading ? '#FF5252' : '#A6F32C'}}><Text style={{fontWeight:'bold', color: '#fff'}} onPress={randomQuote}>{isLoading ? 'Loading...' : 'New Quotes'}</Text></TouchableOpacity>
+                <TouchableOpacity style={{padding: 15, borderRadius: 20, backgroundColor: isLoading ? '#FF5252' : '#A6F32C'}}><Text style={{fontWeight:'bold', color: '#fff'}} onPress={randomQuote}>{isLoading ? 'Loading...' : <Icon name="refresh-outline" size={30}/>}</Text></TouchableOpacity>
             </View>
             <View style={styles.exBtnBox}>
               <TouchableOpacity style={styles.exBtn} onPress={audio}><Icon name="musical-notes-outline" size={30}/></TouchableOpacity>
-              <TouchableOpacity style={styles.exBtn}><Icon name="copy-outline" size={30}/></TouchableOpacity>
+              <TouchableOpacity style={styles.exBtn} onPress={copyToClipboard}><Icon name="copy-outline" size={30}/></TouchableOpacity>
               <TouchableOpacity style={styles.exBtn}><Icon name="share-social-outline" size={30}/></TouchableOpacity>
-              <TouchableOpacity style={styles.exBtn}><Icon name="settings-outline" size={30}/></TouchableOpacity>
+              <TouchableOpacity style={styles.exBtn} onPress={popupWin}><Icon name="settings-outline" size={30}/></TouchableOpacity>
             </View>
         </View>
         
         <StatusBar style="auto" />
+        <Modal transparent={true} visible={window} animationType='fade' onRequestClose={() => {setwindow(false)}}>
+          <View style={styles.modalView}>
+            <View style={styles.cardView}>
+              <Text>This s a Popup Window</Text>
+              <TouchableOpacity style={styles.exBtn} onPress={() => {navigation.navigate('Login')}}><Text>Login</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.exBtn} onPress={() => {setwindow(false)}}><Text>Customize</Text></TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
     </View>
   )
 }
@@ -60,13 +86,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   main: {
-    backgroundColor: '#F4FDE7',
-    height: '50%',
-    width: '85%',
+    backgroundColor: 'white',
+    /*height: '50%',
+    width: '85%',*/
+    margin: 5,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 50,
-    padding: 40,
+    padding: 20,
 
   },
   txtMain: {
@@ -86,6 +113,27 @@ const styles = StyleSheet.create({
   },
   exBtn: {
     padding:20
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  cardView: {
+    margin: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   }
 });
 
