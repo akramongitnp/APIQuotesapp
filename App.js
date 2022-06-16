04afd4ba8d6db48e67c3d751aef49ea6d315cd55
akramongitnp <akramsiddikinp@gmail.com>
2022-06-16 08:19:04 +0530

    Modal and login created few bugs there

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './Components/main'
+import Test from './Components/test'
+import Login from './Components/login'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Funware'>
+        <Stack.Screen name='Funware' component={Main}/>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Test' component={Test}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
